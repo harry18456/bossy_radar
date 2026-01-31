a3f8af38c870100605fdbaec1424b1370bfd273f
harry18456 <harry18456@gmail.com>
2026-01-31 14:27:45 +0800

    style: center adsense unit content

--- frontend/app/components/common/AdSenseUnit.vue
+++ frontend/app/components/common/AdSenseUnit.vue
@@ -91,7 +91,7 @@ onMounted(() => {
     <ClientOnly v-else>
       <ins ref="adElement"
            class="adsbygoogle"
-           style="display:block; width: 100%; min-height: 100px;"
+           style="display:block; width: 100%; min-height: 100px; text-align: center;"
            :data-ad-client="adClient"
            :data-ad-slot="currentAdSlot"
            :data-ad-format="adFormat || 'auto'"
